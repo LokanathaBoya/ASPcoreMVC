df8d1f3ae6cae9ccd6deee82e2b193a5da877fc9
Lokanath <lokanath.boya@gmail.com>
2023-03-09 12:30:52 +0530

    End of the course

--- SchoolManagementApp.MVC/Controllers/ClassesController.cs
+++ SchoolManagementApp.MVC/Controllers/ClassesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -15,10 +16,12 @@ namespace SchoolManagementApp.MVC.Controllers
     public class ClassesController : Controller
     {
         private readonly SchoolManagementDbContext _context;
+        private readonly INotyfService _notyfService;
 
-        public ClassesController(SchoolManagementDbContext context)
+        public ClassesController(SchoolManagementDbContext context , INotyfService notyfService)
         {
-            _context = context;
+             _context = context;
+            _notyfService = notyfService;
         }
 
         // GET: Classes
@@ -206,7 +209,7 @@ namespace SchoolManagementApp.MVC.Controllers
                 enrollment.ClassId = classId;
                 enrollment.StudentId = studentId;
                 await _context.AddAsync(enrollment);
-                // _notyfService.Success($"Student Enrolled Successfully");
+                _notyfService.Success($"Student Enrolled Successfully");
             }
             else
             {
@@ -215,7 +218,7 @@ namespace SchoolManagementApp.MVC.Controllers
 
                 if(enrollment != null){
                     _context.Remove(enrollment);
-                    // _notyfService.Warning($"Student Unenrolled Successfully");
+                    _notyfService.Warning($"Student Unenrolled Successfully");
                 }    
             }
             await _context.SaveChangesAsync();
--- SchoolManagementApp.MVC/Program.cs
+++ SchoolManagementApp.MVC/Program.cs
@@ -1,6 +1,8 @@
 using Auth0.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using SchoolManagementApp.MVC.Data;
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,11 @@ builder.Services.AddDbContext<SchoolManagementDbContext>(opt => opt.UseSqlServer
             options.ClientId = builder.Configuration["Auth0:ClientId"];
         });
 builder.Services.AddControllersWithViews();
+builder.Services.AddNotyf(c => {
+    c.DurationInSeconds =5;
+    c.IsDismissable=true;
+    c.Position=NotyfPosition.TopRight;
+});
 
 var app = builder.Build();
 
@@ -33,7 +40,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseNotyf();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
